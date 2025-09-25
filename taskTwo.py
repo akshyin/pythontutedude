142560d01fd09885623662dd801df45c15224c5b
akshyin <akshay.akshyin@gmail.com>
2025-09-25 09:49:51 +0530

    create fullName variable and assigned the appropriate string in task 2

--- taskTwo.py
+++ taskTwo.py
@@ -1,17 +1,19 @@
-#take the first name from user and assign it to x
-x=input("enter the first name: ")
-#take the last name from the user and assign it to y
-y=input("enter the last name: ")
+#take the first name from user and assign it to firstName
+firstName=input("enter the first name: ")
+#take the last name from the user and assign it to lastName
+lastName=input("enter the last name: ")
 #put two blank line as shown in output
 print('')
 print('')
 #check the input x and y are non empty
-if x=='':
+if firstName=='':
     print("First Name can not be empty")
-elif y=='':
+elif lastName=='':
     print("First Name can not be empty")
 else:  
 #use string print to show the welcome message to the user
 #use string concatenation for including the name in welcome message.
-    print("Hello, "+x+" "+y+"!, Welcome to the python program");
+#create a fullName variable and append firstName and lastName in it
+    fullName=firstName+' '+lastName;
+    print("Hello, "+fullName+"!, Welcome to the python program");
 
