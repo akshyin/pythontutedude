f3d0626138ef38258b0e3672673c5de09a48d422
akshyin <akshay.akshyin@gmail.com>
2025-09-25 08:08:14 +0530

    added comment in task1 and task2

--- taskOne.py
+++ taskOne.py
@@ -1,10 +1,19 @@
-x=input("enter the first number: ")
-y=input("enter the second number: ")
+#Take the first number as input from user and assign it to x variable
+x=input("Enter the first number: ")
+#Take the second number as input from the user and assign it to y variable.
+y=input("Enter the second number: ")
+#Convert x into integer
 x=int(x)
+#Convert y into integer
 y=int(y)
+#Add a new line
 print('')
-print("addition: ",x+y)
-print("subtraction: ",x-y)
-print("multiplication: ",x*y)
-print("divison: ",x/y)
+#Print Addition
+print("Addition: ",x+y)
+#Print Subtraction
+print("Subtraction: ",x-y)
+#Print Multiplication
+print("Multiplication: ",x*y)
+#Print Division
+print("Division: ",x/y)
 
--- taskTwo.py
+++ taskTwo.py
@@ -1,6 +1,11 @@
+#take the first name from user and assign it to x
 x=input("enter the first name: ")
+#take the last name from the user and assign it to y
 y=input("enter the last name: ")
+#put two blank line as shown in output
 print('')
 print('')
+#use string print to show the welcome message to the user
+#use string concatenation for including the name in welcome message.
 print("Hello "+x+" "+y+", Welcome to the python course");
 
