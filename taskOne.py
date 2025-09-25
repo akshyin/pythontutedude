aff2b102e6d02ab448efe8cc7cc9c62b4b00d803
akshyin <akshay.akshyin@gmail.com>
2025-09-25 09:36:24 +0530

    added validation for integer and operation on integer

--- taskOne.py
+++ taskOne.py
@@ -1,19 +1,45 @@
+#Take an indicator for error happened in input
+errorfound=False
 #Take the first number as input from user and assign it to x variable
 x=input("Enter the first number: ")
 #Take the second number as input from the user and assign it to y variable.
 y=input("Enter the second number: ")
 #Convert x into integer
-x=int(x)
+try:
+    x=int(x)
+except ValueError:
+    errorfound=True
+    print("Error in reading first number")
 #Convert y into integer
-y=int(y)
+try:
+    y=int(y)
+except ValueError:
+    errorfound=True
+    print("Error in reading second number")
 #Add a new line
 print('')
 #Print Addition
-print("Addition: ",x+y)
-#Print Subtraction
-print("Subtraction: ",x-y)
-#Print Multiplication
-print("Multiplication: ",x*y)
-#Print Division
-print("Division: ",x/y)
+if not errorfound:
+    try:
+        print("Addition: ",x+y)
+    except:
+        print("Addition can not be done with ", x,y)
+    #Print Subtraction
+    try:
+        print("Subtraction: ",x-y)
+    except:
+        print("Subtraction can not be done with ",x,y)
+    #Print Multiplication
+    try:
+        print("Multiplication: ",x*y)
+    except:
+        print("Multiplication can not be done with ",x,y)
+    #Print Division
+    if y==0:
+        print("Division Error: Not able to divide by zero")
+    else:
+        try:
+            print("Division: ",x/y)
+        except:
+            print("Division can not be done with ",x,y)
 
